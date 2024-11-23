e273c09d276b27e675cf5699889866cb055107c4
grbll@posteo.com <grbll@posteo.com>
2024-11-23 15:52:20 +0100

    added application of cancel function and select statement to contexts and channels.

--- main.go
+++ main.go
@@ -3,23 +3,38 @@ package main
 import (
 	"context"
 	"fmt"
+	"time"
 )
 
 func doSomething(c context.Context) {
-	fmt.Printf("I said hello to %v!\n", c.Value(true))
+	cc, cancelCtx := context.WithCancel(c)
+	var numChan chan int = make(chan int)
 
-	cc := context.WithValue(c, true, "linda")
-	doSomethingElse(cc)
+	go doSomethingElse(numChan, cc)
+	for i := 0; i < 20; i++ {
+		numChan <- i
+	}
+	cancelCtx()
+	time.Sleep(100 * time.Millisecond)
 
-	fmt.Printf("I said hello to %v!\n", c.Value(true))
+	fmt.Printf("doSomething finished.\n")
 }
 
-func doSomethingElse(c context.Context) {
-	fmt.Printf("I said bye to %v!\n", c.Value(true))
+func doSomethingElse(ch <-chan int, c context.Context) {
+	for {
+		select {
+		case <-c.Done():
+			if err := c.Err(); err != nil {
+				fmt.Printf("Error in context: %v\n", err)
+			}
+			fmt.Printf("doSomethingElse finished due to closed context.\n")
+			return
+		case num := <-ch:
+			fmt.Printf("%v was recieved.\n", num)
+		}
+	}
 }
 func main() {
 	c := context.Background()
-
-	c = context.WithValue(c, true, "hans")
 	doSomething(c)
 }
